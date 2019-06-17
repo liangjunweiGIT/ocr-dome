0596596842fc825d617c5b1c6d66f9a92a80ab86
junwei.liang01 <junwei.liang01@ucarinc.com>
2019-06-17 17:33:22 +0800

    Initial commit

---
+++ src/test/java/com/ljw/ocr/OcrDomeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ljw.ocr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OcrDomeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
